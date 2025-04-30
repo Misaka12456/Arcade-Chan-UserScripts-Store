e70a2eaf2d2b15d3fba03b3101e922d50a6e5e91
Mesilane <mesilane@126.com>
2025-04-30 23:52:46 +0800

    Update Rain.lua

--- Rain.lua
+++ Rain.lua
@@ -18,7 +18,7 @@ ScriptInfo = {
         },
         {
             type = "number",
-            name = "雨点间隔",
+            name = "雨点间隔(ms)",
             required = false,
             default = 200
         },
@@ -31,13 +31,13 @@ ScriptInfo = {
             type = "number",
             name = "雨点左x限制",
             required = false,
-			default = -0.25
+            default = -0.25
         },
         {
             type = "number",
             name = "雨点右x限制",
             required = false,
-			default = 1.25
+            default = 1.25
         }
     }
 }
@@ -48,61 +48,50 @@ function ScriptMain(...)
     local endTiming = args[2]
     local interval = args[3] or 200
     local length = args[4] or interval
-	if args[5] == nil then
-		leftx = 0
-	else 
-		leftx = (args[5] + 0.25) * 1000
-	end
-	if args[6] == nil then
-		rightx = 1500
-	else 
-		rightx = (args[6] + 0.25) * 1000
-	end
+
+    if args[5] == nil then
+        leftx = 0
+    else 
+        leftx = (args[5] + 0.25) * 1000
+    end
+    if args[6] == nil then
+        rightx = 1500
+    else 
+        rightx = (args[6] + 0.25) * 1000
+    end
 	--"为后文整数处理埋下伏笔""哦对没错我变量名是偷a1的脚本的"
 
     -- 我不知道还有什么别的问题了，大概（
-    if startTiming >= endTiming then
-        return "错误：起始时间必须早于结束时间"
-    end
-    if interval <= 0 then
-        return "错误：雨点间隔必须大于0"
-    end
-    if length <= 0 then
-        return "错误：雨点长度必须大于0"
-    end
-
-    local totalDuration = endTiming - startTiming
-    local drops = totalDuration / interval
-
-    -- 处理浮点数精度问题并计算实际循环次数
-    local loopCount = Math.Floor(drops)
-    if loopCount < 1 then
-        loopCount = 1  -- 至少生成一个雨滴
-    end
+    if startTiming >= endTiming then return "错误：起始时间必须早于结束时间" end
+    if interval <= 0 then return "错误：雨点间隔必须大于0" end
+    if length <= 0 then return "错误：雨点长度必须大于0" end
+    if leftx > rightx then return "错误：左x限制不能大于右x限制" end
 
     local chart = ChartInstance
+    local loopCount = 0
 
-    for i = 0, loopCount - 1 do
-        local p = (loopCount == 1) and 0 or (i / (loopCount - 1))
-        local t = startTiming + p * totalDuration
-        local endT = t + length
-
-        -- 生成随机坐标(感觉随机小数再处理不如随机整数再/1000简便（）从a1脚本偷学的，感觉非常好）)
+    -- 基于间隔的严格时间递进！这样应该就没有间隔了吧！
+    local currentTiming = startTiming
+    while currentTiming < endTiming do
+        local endT = currentTiming + length
+        
         local randX = Math.RandInt(leftx, rightx)
         local randY = Math.RandInt(0, 1000)
-        local x = (randX - 250) / 1000  -- [-0.25, 1.25]
-        local y = randY / 1000         -- [0.0, 1.0]
+        local x = (randX - 250) / 1000
+        local y = randY / 1000
 
-        -- 创建黑线
         local arc = ScriptArc.Create(
-            t,
-            endT, 
-            0, 
+            currentTiming,
+            endT,
+            0,
             x, x,
             y, y,
             "s",
             true
         )
         chart:AddArc(arc)
+
+        currentTiming = currentTiming + interval
+        loopCount = loopCount + 1
     end
 end
