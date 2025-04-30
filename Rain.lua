19f5eff8fb215ace1f1100335dd74311bf5cf985
Mesilane <mesilane@126.com>
2025-04-30 23:15:09 +0800

    Update Rain.lua

--- Rain.lua
+++ Rain.lua
@@ -30,12 +30,14 @@ ScriptInfo = {
         {
             type = "number",
             name = "雨点左x限制",
-            required = false
+            required = false,
+			default = -0.25
         },
         {
             type = "number",
             name = "雨点右x限制",
-            required = false
+            required = false,
+			default = 1.25
         }
     }
 }
@@ -46,8 +48,16 @@ function ScriptMain(...)
     local endTiming = args[2]
     local interval = args[3] or 200
     local length = args[4] or interval
-	local leftx = (args[5]+0.25)*1000 or 0
-    local rightx = (args[6]+0.25)*1000 or 1500
+	if args[5] == nil then
+		leftx = 0
+	else 
+		leftx = (args[5] + 0.25) * 1000
+	end
+	if args[6] == nil then
+		rightx = 1500
+	else 
+		rightx = (args[6] + 0.25) * 1000
+	end
 	--"为后文整数处理埋下伏笔""哦对没错我变量名是偷a1的脚本的"
 
     -- 我不知道还有什么别的问题了，大概（
@@ -92,8 +102,6 @@ function ScriptMain(...)
             y, y,
             "s",
             true
-			
-			
         )
         chart:AddArc(arc)
     end
